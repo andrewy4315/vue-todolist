fd5958c8bcbe3af754b44695115fc86b59c0c62f
andrewy4315 <andrewy4315@gmail.com>
2024-02-27 22:28:53 -0500

    second commit

--- src/store/index.js
+++ src/store/index.js
@@ -2,13 +2,41 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    todos: [],
+    pinned: []
   },
   mutations: {
+    addTodoMut(state, newTodo) {
+      state.todos.push(newTodo);
+    },
+    deleteTodoMut(state, index) {
+      state.todos.splice(index, 1);
+    },
+    pinTodoMut(state, index) {
+      state.pinned.push(state.todos.splice(index, 1)[0]);
+    },
+    unpinMut(state, index) {
+      state.todos.push(state.pinned.splice(index, 1)[0]);
+    },
+    deletePinnedMut(state, index) {
+      state.pinned.splice(index, 1);
+    },
   },
   actions: {
+    addTodoAct({ commit }, todo) {
+      commit('addTodoMut', todo);
+    },
+    deleteTodoAct({ commit }, index) {
+      commit('deleteTodoMut', index);
+    },
+    pinTodoAct({ commit }, index) {
+      commit('pinTodoMut', index);
+    },
+    unpinAct({ commit }, index) {
+      commit('unpinMut', index);
+    },
+    deletePinnedAct({ commit }, index) {
+      commit('deletePinnedMut', index);
+    },
   },
-  modules: {
-  }
 })
